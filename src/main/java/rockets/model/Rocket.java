bad39622c10c977074b42c2c9e09fcd688ce6e84
Winifred1997 <30387197+Winifred1997@users.noreply.github.com>
2020-08-13 21:24:45 +0800

    Add files via upload

--- src/main/java/rockets/model/Rocket.java
+++ src/main/java/rockets/model/Rocket.java
@@ -3,19 +3,25 @@ package rockets.model;
 import java.util.Objects;
 
 import static org.apache.commons.lang3.Validate.notNull;
+import static org.apache.commons.lang3.Validate.*;
 
 public class Rocket extends Entity {
     private String name;
 
+    private String family;
+
     private String country;
 
-    private LaunchServiceProvider manufacturer;
 
     private String massToLEO;
 
     private String massToGTO;
 
     private String massToOther;
+
+    private Launch launch;
+
+    private LaunchServiceProvider manufacturer;
 
     /**
      * All parameters shouldn't be null.
@@ -25,18 +31,22 @@ public class Rocket extends Entity {
      * @param manufacturer
      */
     public Rocket(String name, String country, LaunchServiceProvider manufacturer) {
-        notNull(name);
-        notNull(country);
-        notNull(manufacturer);
+        notNull(name,"Name cannot be null");
+        notNull(country,"Country cannot be null");
+        notNull(manufacturer,"Manufacturer cannot be null");
 
         this.name = name;
         this.country = country;
         this.manufacturer = manufacturer;
+
     }
+
 
     public String getName() {
         return name;
     }
+
+    public String getFamily(){ return family;}
 
     public String getCountry() {
         return country;
@@ -59,17 +69,31 @@ public class Rocket extends Entity {
     }
 
     public void setMassToLEO(String massToLEO) {
+        notNull(massToLEO,"massToLEO cannot be null");
         this.massToLEO = massToLEO;
     }
 
+    public void setFamily(String family){
+        notNull(family,"Family cannot be null");
+        this.family = family;
+    }
+
     public void setMassToGTO(String massToGTO) {
+        notNull(massToGTO,"Mass cannot be null");
         this.massToGTO = massToGTO;
     }
 
     public void setMassToOther(String massToOther) {
+        notNull(massToOther,"Mass cannot be null");
         this.massToOther = massToOther;
     }
 
+    public Launch getLaunch() {
+        return launch;
+    }
+    public void setLaunch(Launch launch) {
+        this.launch = launch;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
